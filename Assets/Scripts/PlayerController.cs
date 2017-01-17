28b8468ba3eb1d232af5c346abe753382009a460
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-01-17 09:53:56 -0500

    Solution of the bouncing collider and new movement with rigidBody in the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour {
 
 	//Player animator
 	private Animator playerAnim;
+	//Player RigidBody
+	private Rigidbody2D playerRigidBody;
 	//If the player is facing to some direction
 	private bool playerMoving;
 	//Record the las direction of the player
@@ -18,6 +20,8 @@ public class PlayerController : MonoBehaviour {
 
 		//I get the animator in the player
 		playerAnim = GetComponent<Animator> ();
+
+		playerRigidBody = GetComponent<Rigidbody2D> ();
 	
 	}
 	
@@ -30,17 +34,29 @@ public class PlayerController : MonoBehaviour {
 
 		//Moving to the right and the left
 		if(Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f){
-			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			//transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+
+			//With the rigid body i don't need the tranlate and this removes the bounce with the collider
+			playerRigidBody.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * moveSpeed, playerRigidBody.velocity.y);
 			playerMoving = true;
 			lastMovement = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}
 
 		//Moving up and Down
 		if(Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f){
-			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			//transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+			playerRigidBody.velocity = new Vector2 (playerRigidBody.velocity.x, Input.GetAxisRaw ("Vertical") * moveSpeed);
 			playerMoving = true;
 			lastMovement = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
 		}
+
+		//If the player it's not moving it stops
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f)
+			playerRigidBody.velocity = new Vector2 (0f, playerRigidBody.velocity.y);
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f)
+			playerRigidBody.velocity = new Vector2 (playerRigidBody.velocity.x, 0f );
 
 		//Here it's where i ser the animator
 		playerAnim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
