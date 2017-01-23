aa4b9bd8c755bad15ba4b46acfdf4c348ba62bed
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-01-23 12:43:36 -0500

    Added a weapon and a can attack enemies

---
+++ Assets/Scripts/AttackEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//If the sword colliders touch an eenmy it destroys it.
+	void OnTriggerEnter2D(Collider2D collision){
+		
+		if (collision.gameObject.tag == "Enemy"){
+			Destroy (collision.gameObject);
+			Debug.Log ("It's an enemy");
+		}
+	}
+}
