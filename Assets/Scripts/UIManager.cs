61d9216a5cda8e7e953999bd5ccebc071b09f9bf
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-01-31 09:39:16 -0500

    Added an health bar to the player

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Slider healthBar;
+	public Text HPtext;
+	public PlayerHealthManager playerHealth;
+	private static bool UIExist;
+
+	// Use this for initialization
+	void Start () {
+
+		if(!UIExist) {
+			UIExist = true;
+			//Doesn't destroy object when the scene loads
+			DontDestroyOnLoad (transform.gameObject);
+		}
+		else {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Player health in the bar, reference to the PlayerHealthManager
+		healthBar.maxValue = playerHealth.playerMaxHealth;
+		healthBar.value = playerHealth.playerCurrentHealth;
+
+		HPtext.text = "HP: " + playerHealth.playerCurrentHealth + "/" + playerHealth.playerMaxHealth;
+	}
+}
